7423858f6c37a03278a8eaa97861f0e12e82d4b5
simonn70 <simonadjei70@gmail.com>
2024-05-03 19:14:12 -0700

    add small changes

--- lib/connect.ts
+++ lib/connect.ts
@@ -4,7 +4,8 @@ import { connectToDB } from "./db";
 export async function createUser(user: any) {
     try {
       await connectToDB();
-      const newUser = await Mentee.create(user);
+      const newUser =  new Mentee(user)
+      await newUser.save()
       return JSON.parse(JSON.stringify(newUser));
     } catch (error) {
       console.log(error);
