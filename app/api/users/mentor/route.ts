ada73567fe171aeb0b2ec111214984041aa8d556
simonn70 <simonadjei70@gmail.com>
2024-05-08 10:06:41 -0700

    edit mentor api

--- app/api/users/mentor/route.ts
+++ app/api/users/mentor/route.ts
@@ -1,31 +1,31 @@
 import { connectToDB } from "@/lib/db"
 import Mentor from "@/models/Mentor"
-import { User } from "@/models/User"
+import User  from "@/models/User"
+import { currentUser } from "@clerk/nextjs/server"
 
 
 import { NextRequest, NextResponse } from "next/server"
 
 export const POST =async(req:NextRequest)=>{
    try {
-    await connectToDB()
-    const requestData = await req.json()
-    const {email}= requestData
-   const user = await User.findOne({email})
+       const requestData = await req.json()
+       const loggedInClerkUser = await currentUser()
+
+       await connectToDB()
+       const user= await User.findById({clerkId:loggedInClerkUser?.id})   
+   
     if(!user){
         return NextResponse.json({message:'user was not found'})
     }
+    
     const newMentor = new Mentor({
-        user:user._id
-
+        userId:user._id ,
+        ...requestData
     })
-
     await newMentor.save()
 
-    return NextResponse.json({message:'mentor created successfully',newMentor})
+    return NextResponse.json({message:'mentor created successfully',mentor:newMentor})
     
-
-
-  
    } catch (error) {
     return NextResponse.json({ message: 'Failed to create mentor' });
    }
