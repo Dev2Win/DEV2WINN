a89ab9a22ec593b22f3e5fc4a4959903f2ba04ef
estherlardze <estherlardze8@gmail.com>
2024-05-08 07:41:08 +0000

    profile done

---
+++ components/StepFourForm.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Stepper from './Stepper'
+import { IoCheckmarkOutline } from "react-icons/io5";
+
+
+type FormTwo = {
+  onSubmit : () => void;
+  onPrevious: () => void;
+  currentStep: number;
+  complete: boolean;
+  steps: string[];
+}
+
+
+function StepThreeForm({ onSubmit, onPrevious, currentStep, complete, steps} : FormTwo) {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen max-w-md mx-auto '>
+      <h1>User name</h1>
+      <div className='max-w-md'>
+         <Stepper currentStep={currentStep} complete={complete} steps={steps}/>
+       </div>      
+        <div className='my-6 bg-purple-1 rounded-full p-3'>
+          <IoCheckmarkOutline size={25} className='text-white'/>
+        </div>
+       <h1 className='text-3xl font-bold my-3'>Congratulations!</h1>
+      <p className='text-gray-500'>You have finish your onboarding stage</p>
+      <div className='flex justify-between gap-6 w-[80%]'>
+        <button 
+          onClick={onPrevious} 
+          className='bg-purple-1 px-4 py-2 rounded  text-white mt-5 hover:bg-dark-4 transition-all w-[50%]'>
+            previous
+        </button>
+        <button 
+           onClick={onSubmit} 
+           className='bg-purple-1 px-4 py-2 rounded  text-white mt-5 hover:bg-dark-4 transition-all w-[50%]'>
+            finish
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default StepThreeForm
--- components/StepThreeForm.tsx
+++ components/StepThreeForm.tsx
@@ -4,9 +4,18 @@ import React, { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import Stepper from '@/components/Stepper';
 
-type CardType = 'mentor' | 'mentee';
+ type CardType = 'mentor' | 'mentee';
 
-const Page = () => {
+ type FormTwo = {
+  onNext : () => void;
+  onPrevious: () => void;
+  currentStep: number;
+  complete: boolean;
+  steps: string[];
+}
+
+
+function StepFourForm({onNext, onPrevious, currentStep, complete, steps} : FormTwo) {
     const [changeBorder, setChangeBorder] = useState(false);
     const [selectedCard, setSelectedCard] = useState<CardType | null>(null);
   
@@ -19,15 +28,17 @@ const Page = () => {
   return (
     <section className="flex flex-col justify-center items-center h-screen max-w-md mx-auto">
       <h1 className="font-bold text-3xl mb-4">Dev2Win</h1>
-      {/* <Stepper/> */}
+       <div className='max-w-md'>
+         <Stepper currentStep={currentStep} complete={complete} steps={steps}/>
+       </div>
       <h1 className="my-8 font-semibold text-xl">Which user type do you prefer ?</h1>
-      <div className="flex gap-4 my-3">
+      <div className="flex gap-4 my-3 w-[80%]">
         <div
           className={`flex flex-col border ${
             selectedCard === 'mentor'
               ? 'border-purple-1'
               : 'border-black/10'
-          } px-4 py-2 rounded cursor-pointer hover:border-purple-1/60 shadow-sm`}
+          } px-4 py-2 rounded cursor-pointer hover:border-purple-1/60 shadow-sm w-[50%]`}
           onClick={() => handleBorderChange('mentor')}
         >
           <FaUser className={`${selectedCard === 'mentor' ? 'text-purple-1' : 'text-black'} mb-4`} />
@@ -39,17 +50,27 @@ const Page = () => {
             selectedCard === 'mentee'
               ? 'border-purple-1'
               : 'border-black/10'
-          } px-4 py-2 rounded cursor-pointer hover:border-purple-1/60 shadow-sm`}
+          } px-4 py-2 rounded cursor-pointer hover:border-purple-1/60 shadow-sm w-[50%]`}
           onClick={() => handleBorderChange('mentee')}
         >
           <FaUser className={`${selectedCard === 'mentee' ? 'text-purple-1' : 'text-black'} mb-4`} />
           <h1 className="font-semibold">Mentee</h1>
           <p className="text-[14px]">Coach a mentee</p>
         </div>
-      </div>
-      <button className="bg-purple-1 px-4 py-2 rounded w-[80%] text-white mt-5">Continue</button>
-    </section>
+       </div>
+       <div className='flex justify-between items-center gap-8 w-[80%]'>
+        <button 
+          onClick={onPrevious} 
+          className='bg-purple-1 px-4 py-2 rounded  text-white mt-5 hover:bg-dark-4 transition-all w-[50%]'>
+            Previous
+        </button>
+        <button 
+          onClick={onNext} 
+          className='bg-purple-1 px-4 py-2 rounded  text-white mt-5 hover:bg-dark-4 transition-all w-[50%]'>
+            Next
+        </button>
+      </div> </section>
   );
 };
 
-export default Page;
+export default StepFourForm;
