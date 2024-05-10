e41bcee07ec5ce39399a2230aca9813f38dcdd5a
simonn70 <simonadjei70@gmail.com>
2024-05-10 16:13:53 -0700

    add changes

--- app/profile/mentor/page.tsx
+++ app/profile/mentor/page.tsx
@@ -18,7 +18,7 @@ export type FormValues = {
   availability: string;
   expertise: string;
 };
-const mentorUrl: string = process.env.MENTOR_PROFILE_ENDPOINT
+const mentorUrl: string = process.env.MENTOR_PROFILE_ENDPOINT || ""
 // const mentorUrl: string = 'http://localhost:3001/api/users/mentor '
 
 
