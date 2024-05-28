8249d39252edaa0ebb0621dcaf7ddd1d47c34ded
simonn70 <simonadjei70@gmail.com>
2024-05-28 16:45:42 -0700

    add new changes

--- lib/connect.ts
+++ lib/connect.ts
@@ -12,25 +12,25 @@ export async function createUser(user: any) {
     }
 }
 
-export async function deleteUser(user: any) {
-  try {
-    await connectToDB();
-    // had to change the User type here for testing
-    const newUser =  await User.create(user)
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    console.log(error);
-  }
-}
+// export async function deleteUser(user: any) {
+//   try {
+//     await connectToDB();
+//     // had to change the User type here for testing
+//     const newUser =  await User.create(user)
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export async function updateUser(user: any) {
-  try {
-    await connectToDB();
-    // had to change the User type here for testing
-    const newUser =  await User.create(user)
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    console.log(error);
-  }
-}
+// export async function updateUser(user: any) {
+//   try {
+//     await connectToDB();
+//     // had to change the User type here for testing
+//     const newUser =  await User.create(user)
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
