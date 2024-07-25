8109e99830bd55ca6c2155ce444d596844b7c6cb
Vinc5764 <vincentdanso05@gmail.com>
2024-07-25 11:46:06 +0000

    update

--- components/profile/ProfileImageUpload.tsx
+++ components/profile/ProfileImageUpload.tsx
@@ -9,7 +9,7 @@ import { useState } from 'react';
 
 export default function ProfileImageUpload() {
   const [image, setImage] = useState(null);
-  const handleImageUpload = (e) => {
+  const handleImageUpload = (e: any) => {
     const file = e.target.files[0];
     setImage(file);
   };
@@ -46,7 +46,7 @@ export default function ProfileImageUpload() {
   );
 }
 
-function UploadIcon(props) {
+function UploadIcon(props: any) {
   return (
     <svg
       {...props}
@@ -63,26 +63,6 @@ function UploadIcon(props) {
       <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
       <polyline points="17 8 12 3 7 8" />
       <line x1="12" x2="12" y1="3" y2="15" />
-    </svg>
-  );
-}
-
-function XIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
     </svg>
   );
 }
